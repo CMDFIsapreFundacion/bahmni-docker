697711a558fc01fd30b8882d499cb6cf0700e48c
juanjortega <juanjortega@gmail.com>
2024-03-10 14:36:01 -0300

    Update init-db.sh

--- bahmni-standard/initdb.d:/init-db.sh
+++ bahmni-standard/initdb.d:/init-db.sh
@@ -2,19 +2,19 @@
 
 # Esperar a que MySQL esté listo
 echo "Esperando a MySQL..."
-while ! mysqladmin ping -h"${MYSQL_HOST}" --silent; do
+while ! mysqladmin ping -h"${OPENMMRS_MYSQL_HOST}" --silent; do
     sleep 1
 done
 echo "MySQL listo."
 
 # Conectarse a MySQL y ejecutar comandos SQL
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -h "${MYSQL_HOST}" <<-EOSQL
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -h "${OPENMMRS_MYSQL_HOST}" <<-EOSQL
     CREATE DATABASE IF NOT EXISTS \`${NOTIFICACIONES_DATABASE}\`;
     CREATE USER IF NOT EXISTS '${MYSQL_USER_NOTIFICACIONES}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD_NOTIFICACIONES}';
         -- Otorgar todos los privilegios en la base de datos de notificaciones
     GRANT ALL PRIVILEGES ON \`${NOTIFICACIONES_DATABASE}\`.* TO '${MYSQL_USER_NOTIFICACIONES}'@'%';
         -- Otorgar privilegios de lectura en openmsdb
-    GRANT SELECT ON \`${openmsdb}\`.* TO '${MYSQL_USER_NOTIFICACIONES}'@'%';
+    GRANT SELECT ON \`${MYSQL_OPENMRS_DATABASE}\`.* TO '${MYSQL_USER_NOTIFICACIONES}'@'%';
 
     FLUSH PRIVILEGES;
 EOSQL
