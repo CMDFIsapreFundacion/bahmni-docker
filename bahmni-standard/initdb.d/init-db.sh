d8ad828044366e45d08f3d9a80feb16a2f246d83
juanjortega <juanjortega@gmail.com>
2024-03-12 20:16:34 -0300

    d

--- bahmni-standard/initdb.d/init-db.sh
+++ bahmni-standard/initdb.d/init-db.sh
@@ -2,16 +2,16 @@
 
 # Esperar a que MySQL esté listo
 echo "Esperando a MySQL..."
-while ! mysqladmin --defaults-extra-file=/notificacionsql/my.cnf ping --silent; do
+while ! mysqladmin --defaults-extra-file=/script/my.cnf ping --silent; do
     sleep 1
 done
 echo "MySQL listo."
 
 # Crear la base de datos y la tabla.
-mysql --defaults-extra-file=/notificacionsql/my.cnf < "/notificacionsql/create_table_notificacion_ges.sql"
+mysql --defaults-extra-file=/script/my.cnf < "/script/create_table_notificacion_ges.sql"
 
 # Crear usuario y base de datos para notificaciones
-mysql --defaults-extra-file=/notificacionsql/my.cnf -e "CREATE USER '${MYSQL_USER_NOTIFICACIONES}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD_NOTIFICACIONES}';"
-mysql --defaults-extra-file=/notificacionsql/my.cnf -e "CREATE DATABASE IF NOT EXISTS \`${NOTIFICACIONES_DATABASE}\`;"
-mysql --defaults-extra-file=/notificacionsql/my.cnf -e "GRANT ALL PRIVILEGES ON \`${NOTIFICACIONES_DATABASE}\`.* TO '${MYSQL_USER_NOTIFICACIONES}'@'%';"
-mysql --defaults-extra-file=/notificacionsql/my.cnf -e "FLUSH PRIVILEGES;"
+mysql --defaults-extra-file=/script/my.cnf -e "CREATE USER '${MYSQL_USER_NOTIFICACIONES}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD_NOTIFICACIONES}';"
+mysql --defaults-extra-file=/script/my.cnf -e "CREATE DATABASE IF NOT EXISTS \`${NOTIFICACIONES_DATABASE}\`;"
+mysql --defaults-extra-file=/script/my.cnf -e "GRANT ALL PRIVILEGES ON \`${NOTIFICACIONES_DATABASE}\`.* TO '${MYSQL_USER_NOTIFICACIONES}'@'%';"
+mysql --defaults-extra-file=/script/my.cnf -e "FLUSH PRIVILEGES;"
