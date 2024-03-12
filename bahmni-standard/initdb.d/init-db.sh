ccce2ca9d2f707d13231becec5b8465b3cade2d4
juanjortega <juanjortega@gmail.com>
2024-03-12 19:48:41 -0300

    cambios al crear tablas y base de datos

---
+++ bahmni-standard/initdb.d/init-db.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Esperar a que MySQL esté listo
+echo "Esperando a MySQL..."
+while ! mysqladmin --silent ping; do
+    sleep 1
+done
+echo "MySQL listo."
+
+# Crear la base de datos y la tabla.
+mysql --defaults-extra-file=/notificacionsql/my.cnf < "/notificacionsql/create_database_and_table.sql"
+
+# Luego inicia MySQL u otro proceso principal si es necesario
+exec "$@"
+
+mysql --defaults-extra-file=/notificacionsql/my.cnf <<-EOSQL
+    CREATE USER '${MYSQL_USER_NOTIFICACIONES}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD_NOTIFICACIONES}';
+    CREATE DATABASE IF NOT EXISTS \`${NOTIFICACIONES_DATABASE}\`;
+    GRANT ALL PRIVILEGES ON \`${NOTIFICACIONES_DATABASE}\`.* TO '${MYSQL_USER_NOTIFICACIONES}'@'%';
+    FLUSH PRIVILEGES;
+EOSQL
